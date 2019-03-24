2b9595c73c69e89f40925212d4779c093ee1fb76
paing123 <paingpaing4228@gmail.com>
2019-03-24 15:15:31 +0630

    spring-config with annotation

--- src/main/java/com/moehtet/job/MyScheduleJob.java
+++ src/main/java/com/moehtet/job/MyScheduleJob.java
@@ -3,6 +3,7 @@ package com.moehtet.job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.scheduling.quartz.QuartzJobBean;
+import com.moehtet.job.*;
 
 public class MyScheduleJob extends QuartzJobBean{
 	private PrintService printService;
